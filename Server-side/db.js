2b09d901870e025e47b1b60b3645bfe4c21bf7f7
Connor Lance <148902465+connorlance@users.noreply.github.com>
2024-05-14 05:28:08 -0400

    Update db.js

--- Server-side/db.js
+++ Server-side/db.js
@@ -2,7 +2,7 @@ import mysql from "mysql";
 
 const pool = mysql.createPool({
   host: "127.0.0.1",
-  user: "user",
-  password: "password",
-  database: "database",
+  user: "connor",
+  password: "lance",
+  database: "mileagepaycalculator",
 });
