1233afd847081f5b31747d1b529ef6f07a59cbec
Connor Lance <connorlance02@gmail.com>
2024-05-18 07:59:52 -0400

    Convert daily, weekly, monthly, yearly calc functions into 1, added functions for creating a new company table, updating company_names table, and getting company names for remove company dropdown menu

--- Server-side/queries.js
+++ Server-side/queries.js
@@ -21,24 +21,40 @@ function insert_mileage_and_pay(date, mileage_start, mileage_end, pay, company, 
   );
 }
 
-// CALCULATE DAILY
-function daily_total_avg_calculation() {
-  pool.query("SELECT MAX(date) AS max_date FROM mileage_and_pay", (err, results) => {
+//CALCULATE DAILY, WEEKLY, MONTHLY, YEARLY
+function calculateTotalAvg() {
+  const periods = ["daily", "weekly", "monthly", "yearly"];
+
+  const periodMap = {
+    daily: { unit: "day", format: "YYYY-MM-DD" },
+    weekly: { unit: "week", format: "YYYY-MM-DD" },
+    monthly: { unit: "month", format: "YYYY-MM" },
+    yearly: { unit: "year", format: "YYYY" },
+  };
+
+  const max_date_query = "SELECT MAX(date) AS max_date FROM mileage_and_pay";
+
+  pool.query(max_date_query, (err, results) => {
     if (err) {
       console.error("Error getting max date:", err);
-    } else {
-      const max_date = results[0].max_date;
+      return;
+    }
 
-      pool.query("SELECT * FROM mileage_and_pay WHERE date = ?", [max_date], (error, innerResults) => {
-        if (error) {
-          console.error("Error executing inner query:", error);
-          throw error;
+    const max_date = moment(results[0].max_date);
+
+    periods.forEach((period) => {
+      const { unit, format } = periodMap[period];
+      const start_date = moment(max_date).startOf(unit).toDate();
+      const end_date = moment(max_date).endOf(unit).toDate();
+
+      const query = "SELECT * FROM mileage_and_pay WHERE date BETWEEN ? AND ?";
+      pool.query(query, [start_date, end_date], (err, innerResults) => {
+        if (err) {
+          console.error(`Error fetching ${period} data from database:`, err);
+          return;
         }
 
         if (innerResults.length > 0) {
-          let period = "daily";
-          let start_date = innerResults[0].date;
-          let end_date = start_date;
           let total_miles = 0;
           let total_pay = 0;
 
@@ -52,63 +68,15 @@ function daily_total_avg_calculation() {
           if (innerResults.length === 1) {
             insert_total_avg_calculation(period, start_date, end_date, total_miles, total_pay, avg_per_mile);
           } else {
-            total_pay;
             update_total_avg_calculation(period, start_date, total_miles, total_pay, avg_per_mile);
           }
         } else {
-          console.log("No results found for the date:", max_date);
+          console.log(`No results found for the ${period} period.`);
         }
       });
-    }
+    });
   });
 }
-
-//CALCULATE WEEKLY
-function weekly_total_avg_calculation() {
-  pool.query("SELECT MAX(date) AS max_date FROM mileage_and_pay", (err, results) => {
-    if (err) {
-      console.error("Error getting max date:", err);
-    } else {
-      const max_date = results[0].max_date;
-      const start_date = moment(max_date).startOf("week").toDate();
-      const end_date = moment(max_date).endOf("week").toDate();
-
-      pool.query("SELECT * FROM mileage_and_pay WHERE date BETWEEN ? AND ?", [start_date, end_date], (err, innerResults) => {
-        if (err) {
-          console.error("Error fetching weekly data from database:", err);
-        } else {
-          if (innerResults.length > 0) {
-            let period = "weekly";
-            let total_miles = 0;
-            let total_pay = 0;
-
-            innerResults.forEach((row) => {
-              total_miles += row.total_miles;
-              console.log("here is total miles", total_miles);
-              total_pay += parseFloat(row.pay);
-            });
-
-            let avg_per_mile = total_miles !== 0 ? total_pay / total_miles : 0;
-
-            if (innerResults.length === 1) {
-              insert_total_avg_calculation(period, start_date, end_date, total_miles, total_pay, avg_per_mile);
-            } else {
-              update_total_avg_calculation(period, start_date, total_miles, total_pay, avg_per_mile);
-            }
-          } else {
-            console.log("No results found for the week starting from:", start_date);
-          }
-        }
-      });
-    }
-  });
-}
-
-//CALCULATE MONTHLY
-function monthly_total_avg_calculation() {}
-
-//CALCULATE YEARLY
-function yearly_total_avg_calculation() {}
 
 //INSERT ROW INTO 'total_avg_calculation'
 function insert_total_avg_calculation(period, start_date, end_date, total_miles, total_pay, avg_per_mile) {
@@ -140,28 +108,66 @@ function update_total_avg_calculation(period, start_date, total_miles, total_pay
   );
 }
 
-//NOT COMPLETE
-function insert_per_company_total_avg_calculation(company, period, start_date, end_date, total_miles, total_pay, avg_per_mile, callback) {
-  pool.query(
-    "INSERT INTO total_avg_calculation (company, period, start_date, end_date, total_miles, total_pay, avg_per_mile) VALUES (?, ?, ?, ?, ?, ?)",
-    [company, period, start_date, end_date, total_miles, total_pay, avg_per_mile],
-    (err, data) => {
-      if (err) {
-        console.error("Error insert_per_company_total_avg_calculation query:", err);
-      } else {
-        console.log("insert_per_company_total_avg_calculation Query results:", data);
-      }
-      if (callback) {
-        callback(err, data);
-      }
+//create new per_company_total_avg_calculation table
+function create_per_company_total_avg_calculation(companyName, callback) {
+  const tableName = `${companyName}_total_avg_calculation`;
+
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS ${tableName} (
+      ID int(11) NOT NULL AUTO_INCREMENT,
+      period enum('daily','weekly','monthly','yearly') NOT NULL,
+      start_date date NOT NULL,
+      end_date date NOT NULL,
+      total_miles int(11) NOT NULL,
+      total_pay decimal(10,2) NOT NULL,
+      avg_per_mile decimal(10,2) NOT NULL,
+      PRIMARY KEY (ID)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `;
+
+  // Insert company name into company_names table
+  const insertCompanyQuery = `
+    INSERT IGNORE INTO company_names (company_name) VALUES (?);
+  `;
+
+  pool.query(createTableQuery, (err, result) => {
+    if (err) {
+      console.error("Error creating table:", err);
+      callback(err, false);
+    } else {
+      console.log(`Table ${tableName} created successfully.`);
+      // Execute the insert company query
+      pool.query(insertCompanyQuery, [companyName], (err, result) => {
+        if (err) {
+          console.error("Error inserting company name:", err);
+          callback(err, false);
+        } else {
+          console.log(`Company ${companyName} inserted into company_names table.`);
+          callback(null, true);
+        }
+      });
     }
-  );
+  });
+}
+
+// Function to get company names from the company_names table
+function getCompanyNames(callback) {
+  const query = "SELECT company_name FROM company_names";
+  pool.query(query, (err, results) => {
+    if (err) {
+      console.error("Error fetching company names:", err);
+      callback(err, null);
+    } else {
+      const companyNames = results.map((row) => row.company_name);
+      callback(null, companyNames);
+    }
+  });
 }
 
 module.exports = {
   insert_mileage_and_pay,
-  insert_per_company_total_avg_calculation,
+  calculateTotalAvg,
   insert_total_avg_calculation,
-  daily_total_avg_calculation,
-  weekly_total_avg_calculation,
+  create_per_company_total_avg_calculation,
+  getCompanyNames,
 };
