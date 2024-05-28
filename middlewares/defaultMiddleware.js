a98b8e3f677d94c163767d9e5e98db49a2a6e119
Connor Lance <connorlance02@gmail.com>
2024-05-28 05:25:05 -0400

    Added favicon middleware

--- middlewares/defaultMiddleware.js
+++ middlewares/defaultMiddleware.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const favicon = require("serve-favicon");
 const multer = require("multer");
 const morgan = require("morgan");
+const path = require("path");
 
 const router = express.Router();
+
+//favicon
+const faviconPath = path.join(__dirname, "../public/favicon.ico");
+router.use(favicon(faviconPath));
 
 //Configure multer for handling form data
 const upload = multer();
