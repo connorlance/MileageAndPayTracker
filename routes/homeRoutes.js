fe2adb63438b8e365cda9d8963b93298401c4b55
Connor Lance <connorlance02@gmail.com>
2024-08-05 19:08:39 -0400

    Added interval sorted data route

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -6,6 +6,9 @@ const homeControllers = require("../controllers/homeControllers");
 //Route: index
 router.get("/", homeControllers.getIndex);
 
+//Route: interval sorted data
+router.get("/sort/:type", homeControllers.getIntervalSortedData);
+
 //Route: daily info form
 router.post("/dailyInfoForm", customMiddleware.validateDailyInfoForm, homeControllers.postDailyInfoForm);
 
