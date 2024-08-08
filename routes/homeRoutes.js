1540a886fe6a45d908b7417a0d450e0eb9323d2e
Connor Lance <connorlance02@gmail.com>
2024-08-08 19:59:53 -0400

    Added route for deleting MAP rows

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -12,7 +12,10 @@ router.get("/sort/:type", homeControllers.getIntervalSortedData);
 //Route: daily info form
 router.post("/dailyInfoForm", customMiddleware.validateDailyInfoForm, homeControllers.postDailyInfoForm);
 
-// Route: mileage_and_pay rows
+//Route: mileage_and_pay rows
 router.get("/mapRows", homeControllers.getMapRows);
+
+//Route: Delete MAP row by ID
+router.delete("/deleteRow/:id", homeControllers.deleteRow);
 
 module.exports = router;
