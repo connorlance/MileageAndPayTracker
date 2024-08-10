f3f9fd0f432f191759fa4d6b488db422c896bc2c
Connor Lance <connorlance02@gmail.com>
2024-08-10 01:06:29 -0400

    Renamed from optionsRoutes to apiRoutes; Moved all api related routes from previous homeRoutes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const customMiddleware = require("../middlewares/customMiddleware.js");
+const apiControllers = require("../controllers/apiControllers");
+
+// Route: interval sorted data
+router.get("/sort/:type", apiControllers.getIntervalSortedData);
+
+// Route: daily info form
+router.post("/dailyInfoForm", customMiddleware.validateDailyInfoForm, apiControllers.postDailyInfoForm);
+
+// Route: mileage_and_pay rows
+router.get("/mapRows", apiControllers.getMapRows);
+
+// Route: Delete MAP row by ID
+router.delete("/deleteRow/:id", apiControllers.deleteRow);
+
+// Route: company Insert form
+router.post("/createCompany", customMiddleware.validateTrimLetters("insertCompany"), apiControllers.createCompany);
+
+// Route: send company names to the client
+router.get("/companyNames", apiControllers.getCompanyNames);
+
+// Route: remove company name
+router.delete("/removeCompany", customMiddleware.validateTrimLetters("companyName"), apiControllers.removeCompany);
+
+module.exports = router;
